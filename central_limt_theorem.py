572e48e26b8b9d843d354f435db2186575494f50
Hemal-Mamtora <hemalmamtora1@gmail.com>
2021-07-19 17:30:03 +0530

    plot optionally

--- central_limt_theorem.py
+++ central_limt_theorem.py
@@ -4,6 +4,8 @@ from typing import List
 import matplotlib.pyplot as plt
 import matplotlib
 import statistics
+import pandas as pd
+import math
 
 
 def create_population(sample_size: int) -> List[int]:
@@ -69,29 +71,38 @@ def plot_hist(data: List[int],
         plt.text(0.8,
                  0.8 - i,
                  text,
-                 horizontalalignment='center',
-                 verticalalignment='center',
+                 horizontalalignment="center",
+                 verticalalignment="center",
                  transform=ax.transAxes)
         i += 0.05
     plt.grid(True)
     plt.show()
 
 
-def main():
-    """Driver function"""
+def main(plot=True):
+    """Driver Function
+
+    Args:
+        plot (bool, optional): Decide whether to plot or not. Defaults to True.
+    """
     fig, ax = plt.subplots()
     population_size = int(1E5)
     population = create_population(population_size)
-    plot_hist(population,
-              ax,
-              "Value",
-              "Frequency",
-              "Histogram of Population of Random Numbers",
-              [f"population_size={population_size}"])
+    if plot:
+        plot_hist(population,
+                  ax,
+                  "Value",
+                  "Frequency",
+                  "Histogram of Population of Random Numbers",
+                  [f"population_size={population_size}"])
 
-    sample_size_list = [10, 20]
-    sample_count_list = [1000, 5000]
+    population_mean = statistics.mean(population)
+    population_stdev = statistics.stdev(population)
 
+    sample_size_list = [50, 500]
+    sample_count_list = [500, 5000]
+
+    records = []
     for sample_size in sample_size_list:
         for sample_count in sample_count_list:
             sample_mean_list = generate_sample_mean_list(
@@ -102,18 +113,33 @@ def main():
 
             # also called standard dev of sample distribution of sample means
             std_error = round(statistics.stdev(sample_mean_list), 2)
+            if plot:
+                plot_hist(sample_mean_list,
+                          ax,
+                          "Mean Value",
+                          "Frequency",
+                          "Sampling Distribution of Sample Means",
+                          [
+                              f"sample_count={sample_count}",
+                              f"sample_size={sample_size}",
+                              f"mean_of_sample_means={mean_of_sample_means}",
+                              f"std_error={std_error}"])
 
-            plot_hist(sample_mean_list,
-                      ax,
-                      "Mean Value",
-                      "Frequency",
-                      "Sampling Distribution of Sample Means",
-                      [
-                          f"sample_count={sample_count}",
-                          f"sample_size={sample_size}",
-                          f"mean_of_sample_means={mean_of_sample_means}",
-                          f"std_error={std_error}"])
+            record = {
+                "sample_size": sample_size,
+                "sample_count": sample_count,
+                "population_mean": population_mean,
+                "sample_mean": mean_of_sample_means,
+                "population_stdev": population_stdev,
+                "population_stdev_using_formula": std_error*math.sqrt(sample_size),
+                "sample_stdev": std_error,
+            }
+
+            records.append(record)
+
+    df = pd.DataFrame(records)
+    print(df)
 
 
 if __name__ == "__main__":
-    main()
+    main(plot=False)
